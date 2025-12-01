87666e7b308ce3e4a3fb3a5989acb6d264987399
darren yim <darrenyxj@gmail.com>
2025-12-01 20:53:59 +0100

    feat: implement temp config

---
+++ internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+type Config struct {
+	HTTPServerConfig *HTTPServerConfig
+	DBConfig         *DBConfig
+}
+
+type HTTPServerConfig struct {
+	host string
+	port int
+}
+
+type DBConfig struct {
+	host     string
+	port     int
+	user     string
+	password string
+	dbName   string
+}
+
+func (c *DBConfig) Host() string {
+	return c.host
+}
+
+func (c *DBConfig) Port() int {
+	return c.port
+}
+
+func (c *DBConfig) User() string {
+	return c.user
+}
+
+func (c *DBConfig) Password() string {
+	return c.password
+}
+
+func (c *DBConfig) DBName() string {
+	return c.dbName
+}
+
+func LoadConfig() (*Config, error) {
+	httpConfig := &HTTPServerConfig{
+		host: "",
+		port: 8080,
+	}
+	dbConfig := &DBConfig{
+		host:     "localhost",
+		port:     5430,
+		user:     "user_profile_role",
+		password: "postgrespw",
+		dbName:   "user_profile_local",
+	}
+	return &Config{
+		HTTPServerConfig: httpConfig,
+		DBConfig:         dbConfig,
+	}, nil
+}
