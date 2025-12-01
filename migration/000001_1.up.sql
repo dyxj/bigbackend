bfd3da20cffc1efcaa58171b384c660fa3878145
darren yim <darrenyxj@gmail.com>
2025-12-01 17:26:06 +0100

    feat: add user profile migration script

---
+++ migration/000001_1.up.sql
@@ -0,0 +1,14 @@
+BEGIN;
+CREATE TABLE IF NOT EXISTS user_profile
+(
+    id            uuid        NOT NULL,
+    email         TEXT        NOT NULL,
+    first_name    TEXT        NOT NULL,
+    last_name     TEXT        NOT NULL,
+    date_of_birth DATE        NOT NULL,
+    create_time   TIMESTAMPTZ NOT NULL,
+    update_time   TIMESTAMPTZ NOT NULL,
+    CONSTRAINT user_profile_pk PRIMARY KEY (id),
+    CONSTRAINT user_profile_email_uk UNIQUE (email)
+);
+COMMIT;
