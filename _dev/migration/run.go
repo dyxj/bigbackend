1ea8a1801acb0cfc4814d6e94af4f1f5e571513a
darren yim <darrenyxj@gmail.com>
2025-12-22 11:27:52 +0100

    feat: add standalone migration runner

---
+++ _dev/migration/run.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/dyxj/bigbackend/internal/config"
+	"github.com/dyxj/bigbackend/pkg/sqldb"
+)
+
+func main() {
+	// Parse environment variables
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Panicf("failed to load config: %v", err)
+	}
+
+	// Initialize database connection
+	dbConn, err := sqldb.NewDBConn(context.Background(), cfg.DBConfig)
+	if err != nil {
+		log.Panicf("failed to connect to database: %v", err)
+	}
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Printf("failed to close db conn: %v", err)
+		}
+	}()
+
+	// Run database migrations
+	err = sqldb.RunMigration(dbConn, nil)
+	if err != nil {
+		log.Panicf("failed to run database migrations: %v", err)
+	}
+}
