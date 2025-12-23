5ae7f4ac9e2f281f2811636a134cb6b5ba0af36f
darren yim <darrenyxj@gmail.com>
2025-12-23 15:02:40 +0100

    feat: use pointer for validation error

--- internal/userprofile/userprofile_creator.go
+++ internal/userprofile/userprofile_creator.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/dyxj/bigbackend/internal/sqlgen/bigbackend/public/entity"
+	"github.com/dyxj/bigbackend/pkg/errorx"
 	"github.com/dyxj/bigbackend/pkg/sqldb"
 	"go.uber.org/zap"
 )
@@ -23,8 +24,12 @@ func NewCreator(logger *zap.Logger, creatorRepo CreatorRepo, mapper Mapper) Crea
 }
 
 func (c *creator) CreateUserProfileTx(ctx context.Context, tx sqldb.Executable, input UserProfile) (UserProfile, error) {
-	// TODO validation
-	// TODO handler UK violation error
+
+	isValid := input.IsValidForCreate()
+	if !isValid {
+		return UserProfile{}, &errorx.ValidationError{}
+	}
+
 	userProfileEntity := c.mapper.ModelToEntity(input)
 	createdEntity, err := c.creatorRepo.InsertUserProfile(ctx, tx, userProfileEntity)
 	if err != nil {
