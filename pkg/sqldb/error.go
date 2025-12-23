7f3b52ed9bc6bb2a8ee560f254a84ccb63a877b8
darren yim <darrenyxj@gmail.com>
2025-12-23 15:06:24 +0100

    chore: clean up

--- pkg/sqldb/error.go
+++ pkg/sqldb/error.go
@@ -2,7 +2,6 @@ package sqldb
 
 import "github.com/lib/pq"
 
-// IsUniqueViolationError TODO consider extracting violation key
 func IsUniqueViolationError(err *pq.Error) bool {
 	if err.Code.Name() == "unique_violation" {
 		return true
